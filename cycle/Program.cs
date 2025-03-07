6c0c7f90556694ed999a1afd94eaed7e031e2674
rena2169 <sufiyanov.renal@gmail.com>
2025-03-07 03:58:11 +0300

    cycle

---
+++ cycle/Program.cs
@@ -0,0 +1,262 @@
+﻿namespace cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Тема: Циклы
+
+            // Таблица умножения
+
+            //Console.WriteLine("Введите число для вывода таблицы умножения: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"Таблица умножения для числа {inputNumber}: ");
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine($"{i} * {inputNumber} = {i * inputNumber}");
+            //}
+
+            // Сумма четных чисел
+
+            //Console.WriteLine("Введите число N: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+
+            //for (int i = 2; i <= inputNumber; i += 2)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+
+            //Console.WriteLine($"Сумма всех четных чисел от 1 до {inputNumber} равна {sum}");
+
+            // Числа Фибоначчи
+
+            //Console.WriteLine("Введите количество чисел Фибоначчи для вывода: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            //if (inputNumber <= 0)
+            //{
+            //    Console.WriteLine("Введите положительное число.");
+            //}
+            //else
+            //{
+            //    int a = 1;
+            //    int b = 1;
+            //    int sumAB = 0;
+
+            //    for (int i = 0; i < inputNumber; i++)
+            //    {
+            //        Console.Write(a + " ");
+            //        sumAB = a + b;
+            //        a = b;
+            //        b = sumAB;
+            //    }
+            //}
+
+            // Генерация случайных чисел
+
+            //Random random = new Random();
+            //int randomNumber;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    randomNumber = random.Next(0, 101);
+            //    if (randomNumber > 50)
+            //    {
+            //        Console.WriteLine(randomNumber);
+            //    }
+            //}
+
+            // Обратный отсчет
+
+            //Console.Write("Введите число: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = inputNumber; i > 0; i--)
+            //{
+            //    Console.Write(i);
+            //}
+
+            // Проверка числа на палиндром
+
+            //Console.Write("Введите число: ");
+            //string inputNumber = Console.ReadLine();
+            //bool isPalindrome = true;
+
+            //for (int i = 0; i < inputNumber.Length / 2; i++)
+            //{
+            //    if (inputNumber[i] != inputNumber[inputNumber.Length - 1 - i])
+            //    {
+            //        isPalindrome = false;
+            //        break;
+            //    }
+            //}
+            //if (isPalindrome)
+            //{
+            //    Console.WriteLine($"{inputNumber} является палиндромом.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{inputNumber} не является палиндромом.");
+            //}
+
+            // Проверка на простое число
+
+            //Console.WriteLine("Введите число: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+            //bool isPrime = true;
+
+            //if (inputNumber < 1)
+            //{
+            //    Console.WriteLine("Введите целое число больше 1: ");
+            //}
+
+            //for (int i = 2; i < Math.Sqrt(inputNumber); i++)
+            //{
+            //    if (inputNumber % i == 0)
+            //    {
+            //        isPrime = false;
+            //        break;
+            //    }
+            //}
+            //if (isPrime)
+            //{
+            //    Console.WriteLine("Число простое");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Число составное");
+            //}// Тема: Циклы
+
+            // Таблица умножения
+
+            //Console.WriteLine("Введите число для вывода таблицы умножения: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"Таблица умножения для числа {inputNumber}: ");
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine($"{i} * {inputNumber} = {i * inputNumber}");
+            //}
+
+            // Сумма четных чисел
+
+            //Console.WriteLine("Введите число N: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+
+            //for (int i = 2; i <= inputNumber; i += 2)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+
+            //Console.WriteLine($"Сумма всех четных чисел от 1 до {inputNumber} равна {sum}");
+
+            // Числа Фибоначчи
+
+            //Console.WriteLine("Введите количество чисел Фибоначчи для вывода: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            //if (inputNumber <= 0)
+            //{
+            //    Console.WriteLine("Введите положительное число.");
+            //}
+            //else
+            //{
+            //    int a = 1;
+            //    int b = 1;
+            //    int sumAB = 0;
+
+            //    for (int i = 0; i < inputNumber; i++)
+            //    {
+            //        Console.Write(a + " ");
+            //        sumAB = a + b;
+            //        a = b;
+            //        b = sumAB;
+            //    }
+            //}
+
+            // Генерация случайных чисел
+
+            //Random random = new Random();
+            //int randomNumber;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    randomNumber = random.Next(0, 101);
+            //    if (randomNumber > 50)
+            //    {
+            //        Console.WriteLine(randomNumber);
+            //    }
+            //}
+
+            // Обратный отсчет
+
+            //Console.Write("Введите число: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = inputNumber; i > 0; i--)
+            //{
+            //    Console.Write(i);
+            //}
+
+            // Проверка числа на палиндром
+
+            //Console.Write("Введите число: ");
+            //string inputNumber = Console.ReadLine();
+            //bool isPalindrome = true;
+
+            //for (int i = 0; i < inputNumber.Length / 2; i++)
+            //{
+            //    if (inputNumber[i] != inputNumber[inputNumber.Length - 1 - i])
+            //    {
+            //        isPalindrome = false;
+            //        break;
+            //    }
+            //}
+            //if (isPalindrome)
+            //{
+            //    Console.WriteLine($"{inputNumber} является палиндромом.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{inputNumber} не является палиндромом.");
+            //}
+
+            // Проверка на простое число
+
+            //Console.WriteLine("Введите число: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+            //bool isPrime = true;
+
+            //if (inputNumber < 1)
+            //{
+            //    Console.WriteLine("Введите целое число больше 1: ");
+            //}
+
+            //for (int i = 2; i < Math.Sqrt(inputNumber); i++)
+            //{
+            //    if (inputNumber % i == 0)
+            //    {
+            //        isPrime = false;
+            //        break;
+            //    }
+            //}
+            //if (isPrime)
+            //{
+            //    Console.WriteLine("Число простое");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Число составное");
+            //}
+        }
+    }
+}
